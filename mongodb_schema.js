36cc2c7d2e7bb707fb1372352b99e3469fca7b3f
Benjamin Wu <benngfour@gmail.com>
2021-03-15 23:15:14 -0700

    added constraints to both schemas

--- mongodb_schema.js
+++ mongodb_schema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const Product_Question = new Schema({
+const ProductQuestionSchema = new Schema({
   product_id: Integer,
   questions: [
     {
@@ -12,11 +12,12 @@ const Product_Question = new Schema({
   ]
 });
 
-const Questions = new Schema({
+const QuestionsSchema = new Schema({
   question_id: Integer,
   question_body: String,
-  question_date: Date ,
+  question_date: Date,
   asker_name: String,
+  asker_email: String
   question_helpfulness: Integer,
   reported: Boolean,
   answers: [
@@ -26,12 +27,22 @@ const Questions = new Schema({
     }
   ]
 });
+QuestionsSchema.path('question_body').validate(function(question_body) {
+  return question_body &&  question_body.length === 1000;
+}, 'Your question cannot excceed 1000 characters');
+QuestionsSchema.path('asker_name').validate(function(asker_name) {
+  return asker_name &&  asker_name.length === 60;
+}, 'Your name cannot excceed 60 characters');
+QuestionsSchema.path('asker_email').validate(function(asker_email) {
+  return asker_email &&  asker_email.length === 60;
+}, 'Your email cannot excceed 60 characters');
 
-const Answers = new Schema({
+const AnswersSchema = new Schema({
   answer_id: Integer,
   answer_body: String,
   answer_date: Date ,
   answerer_name: String,
+  answerer_email: String,
   answer_helpfulness: Integer,
   reported: Boolean,
   photos: [
@@ -40,5 +51,13 @@ const Answers = new Schema({
       url: String,
     }
   ],
-  ]
 });
+AnswersSchema.path('answer_body').validate(function(answer_body) {
+  return answer_body &&  answer_body.length === 1000;
+}, 'Your answer cannot excceed 1000 characters');
+AnswersSchema.path('answerer_name').validate(function(answerer_name) {
+  return answerer_name &&  answerer_name.length === 60;
+}, 'Your name cannot excceed 60 characters');
+AnswersSchema.path('answerer_email').validate(function(answerer_email) {
+  return answerer_email &&  asker_email.length === 60;
+}, 'Your email cannot excceed 60 characters');
--- sqldb_schema.sql
+++ sqldb_schema.sql
@@ -4,9 +4,10 @@
 CREATE TABLE IF NOT EXISTS Questions
 (
     question_id INT NOT NULL PRIMARY KEY,
-    question_body TEXT NOT NULL,
+    question_body TEXT NOT NULL CONSTRAINT q_bodychk CHECK (char_length(question_body) <= 1000),
     question_date DATE NOT NULL,
-    asker_name VARCHAR(50) NOT NULL,
+    asker_name VARCHAR(60) NOT NULL,
+    asker_email VARCHAR(60) NOT NULL,
     question_helpfulness INT,
     reported BOOLEAN NOT NULL,
     product_id INT NOT NULL,
@@ -15,9 +16,10 @@ CREATE TABLE IF NOT EXISTS Questions
 CREATE TABLE IF NOT EXISTS Answers
 (
     answer_id INT NOT NULL PRIMARY KEY,
-    answer_body TEXT NOT NULL,
+    answer_body TEXT NOT NULL CONSTRAINT a_bodychk CHECK (char_length(answer_body) <= 1000),
     answer_date DATE NOT NULL,
-    answerer_name VARCHAR(50) NOT NULL,
+    answerer_name VARCHAR(60) NOT NULL,
+    answerer_email VARCHAR(60) NOT NULL,
     answer_helpfulness INT,
     reported BOOLEAN NOT NULL,
     question_id INT NOT NULL,
